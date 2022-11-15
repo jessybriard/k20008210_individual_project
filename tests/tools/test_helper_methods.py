1546a4ac15a6394d07a2f04195eed61994827d52
Jessy BRIARD <jessy.briard@hotmail.com>
2022-11-15 01:54:17 +0000

    Add unit tests for method extract_returns_from_dataframe()
    - Create test class TestHelperMethods in file test_helper_methods.py
    - Add unit tests for method extract_returns_from_dataframe() to test class TestHelperMethods

---
+++ tests/tools/test_helper_methods.py
@@ -0,0 +1,95 @@
+"""Tests for methods in file helper_methods.py."""
+
+import math
+from unittest import TestCase
+
+import pandas as pd
+
+from src.tools.helper_methods import extract_returns_from_dataframe
+
+
+class TestHelperMethods(TestCase):
+    """Test class for methods in file helper_methods.py."""
+
+    def test_extract_returns_from_dataframe_no_nan(self):
+
+        # Arrange
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-03", "2022-11-04", "2022-11-07"],
+                "Open": [89, 88, 91],
+                "Close": [88, 92, 91],
+            }
+        )
+        data = data.set_index("Date")
+        data.index = pd.DatetimeIndex(data.index)
+
+        # Act
+        returns_series = extract_returns_from_dataframe(data=data)
+
+        # Assert
+        expected_returns_series = pd.Series(
+            data={"2022-11-03": False, "2022-11-04": True, "2022-11-07": False}
+        )
+        expected_returns_series.index = pd.DatetimeIndex(
+            expected_returns_series.index, name="Date"
+        )
+        self.assertTrue(expected_returns_series.equals(returns_series))
+
+    def test_extract_returns_from_dataframe_open_nan(self):
+
+        # Arrange
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-03", "2022-11-04", "2022-11-07"],
+                "Open": [89, math.nan, 91],
+                "Close": [88, 92, 91],
+            }
+        )
+        data = data.set_index("Date")
+        data.index = pd.DatetimeIndex(data.index)
+
+        # Act
+        returns_series = extract_returns_from_dataframe(data=data)
+
+        # Assert
+        expected_returns_series = pd.Series(
+            data={
+                "2022-11-03": False,
+                "2022-11-04": math.nan,
+                "2022-11-07": False,
+            }
+        )
+        expected_returns_series.index = pd.DatetimeIndex(
+            expected_returns_series.index, name="Date"
+        )
+        self.assertTrue(expected_returns_series.equals(returns_series))
+
+    def test_extract_returns_from_dataframe_close_nan(self):
+
+        # Arrange
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-03", "2022-11-04", "2022-11-07"],
+                "Open": [89, 88, 91],
+                "Close": [88, 92, math.nan],
+            }
+        )
+        data = data.set_index("Date")
+        data.index = pd.DatetimeIndex(data.index)
+
+        # Act
+        returns_series = extract_returns_from_dataframe(data=data)
+
+        # Assert
+        expected_returns_series = pd.Series(
+            data={
+                "2022-11-03": False,
+                "2022-11-04": True,
+                "2022-11-07": math.nan,
+            }
+        )
+        expected_returns_series.index = pd.DatetimeIndex(
+            expected_returns_series.index, name="Date"
+        )
+        self.assertTrue(expected_returns_series.equals(returns_series))
