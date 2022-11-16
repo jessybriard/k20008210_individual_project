493c71787c47edc63dcd7c68b553f932d208aaa1
Jessy BRIARD <jessy.briard@hotmail.com>
2022-11-16 18:22:59 +0000

    Add method create_labeled_data_sector_approach() to file labeled_data_builder/time_series_forecasting.py

--- src/tools/labeled_data_builder/time_series_forecasting.py
+++ src/tools/labeled_data_builder/time_series_forecasting.py
@@ -1,6 +1,7 @@
 """Methods to build labeled data from a DataFrame for time series forecasting."""
 
 import math
+from typing import List
 
 import pandas as pd
 
@@ -12,8 +13,7 @@ def create_labeled_data_individual_approach(ticker: str, data: pd.DataFrame, fea
     Args:
         ticker (str): The code for the asset we create labeled data for.
         data (pd.DataFrame): The historical time series for the ticker.
-        features_length (int): The number of previous binary returns to use as features to predict the next one
-            (the label).
+        features_length (int): The number of previous rows to use as features to predict the next one (the label).
 
     Returns:
         labeled_data (pd.DataFrame): The created labeled data, contains a column for features and a column for label.
@@ -33,4 +33,44 @@ def create_labeled_data_individual_approach(ticker: str, data: pd.DataFrame, fea
         if True not in [math.isnan(value) for value in values[i : i + features_length + 1]]:
             features.append(values[i : i + features_length])
             label.append(values[i + features_length])
+    return pd.DataFrame(data={"features": features, "label": label})
+
+
+def create_labeled_data_sector_approach(
+    ticker_label: str, tickers_features: List[str], data: pd.DataFrame, features_length: int
+) -> pd.DataFrame:
+    """Create labeled data for time series forecasting, using given historical data for a ticker, using features_length
+    previous values in the time series as features and the next value as label.
+
+    Args:
+        ticker_label (str): The code for the asset we want to predict for (the label).
+        tickers_features (List[str]): The codes for the assets we want to use as features for the prediction.
+        data (pd.DataFrame): The historical time series for the tickers.
+        features_length (int): The number of previous rows to use as features to predict the next one (the label).
+
+    Returns:
+        labeled_data (pd.DataFrame): The created labeled data, contains a column for features and a column for label.
+
+    """
+
+    if ticker_label not in data.columns:
+        raise ValueError("Parameter 'ticker_label' must represent a valid column in the 'data' provided as parameter.")
+
+    if not tickers_features or not set(data.columns).issuperset(set(tickers_features)):
+        raise ValueError(
+            "Parameter 'tickers_features' must represent valid columns in the 'data' provided as parameter."
+        )
+
+    if features_length < 1:
+        raise ValueError("Parameter 'features_length' must be a strictly positive integer (>= 1).")
+
+    features = []
+    label = []
+    for i in range(len(data) - features_length):
+        features_data_slice = data[tickers_features].iloc[i : i + features_length]
+        if not math.isnan(data[ticker_label].iloc[i + features_length]) and True not in [
+            math.isnan(value) for value in features_data_slice.values.flatten()
+        ]:
+            features.append(list(features_data_slice.values.flatten()))
+            label.append(data[ticker_label].iloc[i + features_length])
     return pd.DataFrame(data={"features": features, "label": label})
