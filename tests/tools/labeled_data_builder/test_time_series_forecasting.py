dd874a5ae20942583ef6105f8836fe55794a9f72
Jessy BRIARD <jessy.briard@hotmail.com>
2022-11-16 19:35:45 +0000

    Add tests for method create_labeled_data_sector_approach() to test_time_series_forecasting.py

--- tests/tools/labeled_data_builder/test_time_series_forecasting.py
+++ tests/tools/labeled_data_builder/test_time_series_forecasting.py
@@ -5,7 +5,10 @@ from unittest import TestCase
 
 import pandas as pd
 
-from src.tools.labeled_data_builder.time_series_forecasting import create_labeled_data_individual_approach
+from src.tools.labeled_data_builder.time_series_forecasting import (
+    create_labeled_data_individual_approach,
+    create_labeled_data_sector_approach,
+)
 
 
 class TestLabeledDataBuilderTimeSeriesForecasting(TestCase):
@@ -264,4 +267,353 @@ class TestLabeledDataBuilderTimeSeriesForecasting(TestCase):
 
         # Assert
         expected_labeled_data = pd.DataFrame(data={"features": [], "label": []})
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    # Tests for method create_labeled_data_sector_approach()
+
+    def test_create_labeled_data_sector_approach_ticker_label_empty_str(self):
+
+        # Arrange
+        ticker_label = ""
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 2
+
+        # Act / Assert
+        with self.assertRaises(ValueError) as e:
+            create_labeled_data_sector_approach(
+                ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+            )
+        self.assertEqual(
+            str(e.exception),
+            "Parameter 'ticker_label' must represent a valid column in the 'data' provided as parameter.",
+        )
+
+    def test_create_labeled_data_sector_approach_ticker_label_not_in_data_columns(self):
+
+        # Arrange
+        ticker_label = "GBPUSD=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 2
+
+        # Act / Assert
+        with self.assertRaises(ValueError) as e:
+            create_labeled_data_sector_approach(
+                ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+            )
+        self.assertEqual(
+            str(e.exception),
+            "Parameter 'ticker_label' must represent a valid column in the 'data' provided as parameter.",
+        )
+
+    def test_create_labeled_data_sector_approach_tickers_features_empty_list(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = []
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 2
+
+        # Act / Assert
+        with self.assertRaises(ValueError) as e:
+            create_labeled_data_sector_approach(
+                ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+            )
+        self.assertEqual(
+            str(e.exception),
+            "Parameter 'tickers_features' must represent valid columns in the 'data' provided as parameter.",
+        )
+
+    def test_create_labeled_data_sector_approach_tickers_features_not_subset_of_data_columns(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "GC=F"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 2
+
+        # Act / Assert
+        with self.assertRaises(ValueError) as e:
+            create_labeled_data_sector_approach(
+                ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+            )
+        self.assertEqual(
+            str(e.exception),
+            "Parameter 'tickers_features' must represent valid columns in the 'data' provided as parameter.",
+        )
+
+    def test_create_labeled_data_sector_approach_features_length_zero(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, False, False, False, False, False],
+            }
+        )
+        features_length = 0
+
+        # Act / Assert
+        with self.assertRaises(ValueError) as e:
+            create_labeled_data_sector_approach(
+                ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+            )
+        self.assertEqual(str(e.exception), "Parameter 'features_length' must be a strictly positive integer (>= 1).")
+
+    def test_create_labeled_data_sector_approach_features_length_negative(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = -1
+
+        # Act / Assert
+        with self.assertRaises(ValueError) as e:
+            create_labeled_data_sector_approach(
+                ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+            )
+        self.assertEqual(str(e.exception), "Parameter 'features_length' must be a strictly positive integer (>= 1).")
+
+    def test_create_labeled_data_sector_approach_nan_in_features(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, math.nan, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 2
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(
+            data={"features": [[False, False, True, True], [True, True, True, False]], "label": [False, True]}
+        )
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    def test_create_labeled_data_sector_approach_nan_in_label(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [math.nan, True, False, math.nan, False, True],
+            }
+        )
+        features_length = 2
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(
+            data={"features": [[True, False], [False, True], [True, True]], "label": [False, False, True]}
+        )
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    def test_create_labeled_data_sector_approach_data_empty(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": [],
+                "CL=F": [],
+                "EUR=X": [],
+            }
+        )
+        features_length = 2
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(data={"features": [], "label": []})
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    def test_create_labeled_data_sector_approach_data_nan_only(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [math.nan, math.nan, math.nan, math.nan, math.nan, math.nan],
+                "EUR=X": [math.nan, math.nan, math.nan, math.nan, math.nan, math.nan],
+            }
+        )
+        features_length = 2
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(data={"features": [], "label": []})
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    def test_create_labeled_data_sector_approach_one_example(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 5
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(
+            data={
+                "features": [
+                    [True, False, False, True, False, False, True, True, True, False],
+                ],
+                "label": [True],
+            }
+        )
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    def test_create_labeled_data_sector_approach_zero_example(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 6
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(data={"features": [], "label": []})
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    def test_create_labeled_data_sector_approach_ticker_label_in_tickers_features(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F", "EUR=X"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 3
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(
+            data={
+                "features": [
+                    [True, False, False, True, False, False],
+                    [False, True, False, False, True, True],
+                    [False, False, True, True, True, False],
+                ],
+                "label": [True, False, True],
+            }
+        )
+        self.assertTrue(expected_labeled_data.equals(labeled_data))
+
+    def test_create_labeled_data_sector_approach_ticker_label_not_in_tickers_features(self):
+
+        # Arrange
+        ticker_label = "EUR=X"
+        tickers_features = ["CL=F"]
+        data = pd.DataFrame(
+            data={
+                "Date": ["2022-11-07", "2022-11-08", "2022-11-09", "2022-11-10", "2022-11-11", "2022-11-14"],
+                "CL=F": [True, False, False, True, True, False],
+                "EUR=X": [False, True, False, True, False, True],
+            }
+        )
+        features_length = 3
+
+        # Act
+        labeled_data = create_labeled_data_sector_approach(
+            ticker_label=ticker_label, tickers_features=tickers_features, data=data, features_length=features_length
+        )
+
+        # Assert
+        expected_labeled_data = pd.DataFrame(
+            data={
+                "features": [[True, False, False], [False, False, True], [False, True, True]],
+                "label": [True, False, True],
+            }
+        )
         self.assertTrue(expected_labeled_data.equals(labeled_data))
