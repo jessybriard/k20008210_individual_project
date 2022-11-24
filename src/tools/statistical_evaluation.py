6466c2bd3a9495357d1b3aa4a1d09d1cb6049225
Jessy BRIARD <jessy.briard@hotmail.com>
2022-11-24 20:22:07 +0000

    Refactor class ClassificationEvaluation and add method f1_score()
    - Refactor class ClassificationEvaluation to use the constructor to pass label datasets and check their length
    - Add method _validate_predictions_length() to raise an exception if y_true and y_predicted have different lengths
    - Add method f1_score() to calculate the F1 Score of the prediction for reach possible label value (True and False)
    - Add package scikit-learn to requirements.txt

--- src/tools/statistical_evaluation.py
+++ src/tools/statistical_evaluation.py
@@ -2,6 +2,8 @@
 
 from typing import List
 
+from sklearn import metrics
+
 
 class ClassificationEvaluation:
     """Class Classification Evaluation.
@@ -10,19 +12,35 @@ class ClassificationEvaluation:
     Classification models.
     """
 
+    def __init__(self, y_true: List[bool], y_predicted: List[bool]):
+        self._validate_predictions_length(y_true, y_predicted)
+        self.y_true = y_true
+        self.y_predicted = y_predicted
+
     @staticmethod
-    def accuracy(y_true: List[bool], y_predicted: List[bool]) -> float:
-        """Calculate the accuracy of the prediction.
+    def _validate_predictions_length(y_true, y_predicted):
+        """Check that true values and predicted values are lists of same length, otherwise raise an Exception.
 
         Args:
             y_true (List[Any]): The true values for the predicted label.
             y_predicted (List[Any]): The predicted values for the label.
+        """
+        if len(y_true) != len(y_predicted):
+            raise ValueError("Parameters 'y_true' and 'y_predicted' must be lists of same length.")
+
+    def accuracy(self) -> float:
+        """Calculate the Accuracy of the prediction.
 
         Returns:
             accuracy (float): The value of the prediction's accuracy.
         """
+        return len([True for i in range(len(self.y_true)) if self.y_true[i] == self.y_predicted[i]]) / len(self.y_true)
 
-        if len(y_true) != len(y_predicted):
-            raise ValueError("Parameters 'y_true' and 'y_predicted' must be lists of same length.")
+    def f1_score(self) -> dict:
+        """Calculate the F1 Score of the prediction, for each possible label value.
 
-        return len([True for i in range(len(y_true)) if y_true[i] == y_predicted[i]]) / len(y_true)
+        Returns:
+            f1_scores (dict): The value of the prediction's F1 Score, for each possible label value.
+        """
+        labels_score = metrics.f1_score(y_true=self.y_true, y_pred=self.y_predicted, labels=[True, False], average=None)
+        return {True: labels_score[0], False: labels_score[1]}
